ead7b43b21bde2316e8e5377e020610a7ab2f5e5
Eiron Rabbit <iamironrabbit@gmail.com>
2019-02-08 16:04:06 -0500

    rm unneeded script

--- build_olm_lib.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-rm -rf olm
-git clone http://git.matrix.org/git/olm.git/
-cd olm/android
-echo ndk.dir=$NDK > local.properties
-./gradlew assembleRelease
-
-cd ../../keanuapp-android
-cp ../olm/android/olm-sdk/build/outputs/aar/olm-sdk-release-*.aar app/libs/olm-sdk.aar
