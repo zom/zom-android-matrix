b2f27ddd3b597cd2ba7e4e02c2f965c8afbef1ee
n8fr8 <nathan@guardianproject.info>
2020-09-17 09:49:21 -0400

    add appupdater

--- app/src/main/java/info/guardianproject/keanuapp/MainActivity.java
+++ app/src/main/java/info/guardianproject/keanuapp/MainActivity.java
@@ -1171,39 +1171,42 @@ public class MainActivity extends BaseActivity {
 
     private void checkForUpdates() {
         // Remove this for store builds!
-     //   UpdateManager.register(this, ImApp.HOCKEY_APP_ID);
 
         //only check github for updates if there is no Google Play
-        if (!hasGooglePlay()) {
-            try {
 
-                String version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        try {
 
-                //if this is a full release, without -beta -rc etc, then check the appupdater!
-                if (version.indexOf("-") == -1) {
+            String version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
 
-                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-                    long timeNow = new Date().getTime();
-                    long timeSinceLastCheck = prefs.getLong("updatetime", -1);
+            //if this is a full release, without -beta -rc etc, then check the appupdater!
+            if (version.indexOf("-") == -1) {
 
-                    //only check for updates once per day
-                    if (timeSinceLastCheck == -1 || (timeNow - timeSinceLastCheck) > 86400) {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                long timeNow = new Date().getTime();
+                long timeSinceLastCheck = prefs.getLong("updatetime", -1);
 
-                        AppUpdater appUpdater = new AppUpdater(this);
-                        appUpdater.setDisplay(Display.DIALOG);
+                //only check for updates once per day
+                if (timeSinceLastCheck == -1 || (timeNow - timeSinceLastCheck) > 86400) {
+
+                    AppUpdater appUpdater = new AppUpdater(this);
+                    appUpdater.setDisplay(Display.SNACKBAR);
+                    if (hasGooglePlay())
+                        appUpdater.setUpdateFrom(UpdateFrom.GOOGLE_PLAY);
+                    else {
                         appUpdater.setUpdateFrom(UpdateFrom.XML);
                         appUpdater.setUpdateXML(ImApp.URL_UPDATER);
-
-                        //  appUpdater.showAppUpdated(true);
-                        appUpdater.start();
-
-                        prefs.edit().putLong("updatetime", timeNow).commit();
                     }
+
+                    appUpdater.showAppUpdated(true);
+                    appUpdater.start();
+
+                    prefs.edit().putLong("updatetime", timeNow).commit();
                 }
-            } catch (Exception e) {
-                Log.d("AppUpdater", "error checking app updates", e);
             }
+        } catch (Exception e) {
+            Log.d("AppUpdater", "error checking app updates", e);
         }
+
     }
 
     boolean hasGooglePlay() {
